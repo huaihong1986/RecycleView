2dabba68d3f435a8aedc4abe2c533389935db352
huaihong <huaihong1986@sina.com>
2016-07-01 12:57:16 +0800

    Initial commit

---
+++ app/src/main/java/com/cvn/huaihong/recycleview/MainActivity.java
@@ -0,0 +1,63 @@
+package com.cvn.huaihong.recycleview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import com.cvn.huaihong.recycleview.R;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+
+public class MainActivity extends AppCompatActivity implements Adapter.IonSlidingViewClickListener {
+    private SlidingMenu mSlidingMenu;
+
+    private RecyclerView mRecyclerView;
+
+    private Adapter mAdapter;
+
+    private final String TAG = "test";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        setAdapter();
+
+    }
+
+    private void initView(){
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+    }
+
+    private void setAdapter(){
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mAdapter = new Adapter(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+    }
+
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Log.i(TAG,"����"+position);
+    }
+
+    @Override
+    public void onDeleteBtnClick(View view, int position) {
+        Log.i(TAG,"ɾ���"+position);
+        mAdapter.removeData(position);
+    }
+    @Override
+    public void onModifyBtnClick(View view, int position) {
+        Log.i(TAG,"ɾ���"+position);
+        mAdapter.modifyData(position,++position);
+    }
+}
